93c5ba6848a1f2adbf8cb254f64eebb5120085e5
Penrosec <penrosec@uregina.ca>
2017-10-11 20:50:16 -0600

    Initial commit

---
+++ Source/PNPV2/enemyController.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "enemyController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PNPV2_API AenemyController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
